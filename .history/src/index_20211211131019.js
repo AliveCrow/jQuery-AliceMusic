dbfbac8c3952c1f04d93f18cc7eb98647fa4618b
grit <136907108@qq.com>
2022-04-26 23:06:01 +0800

    page

--- .history/src/index_20211211131019.js
+++
@@ -1,5 +0,0 @@
-import '../scss/app.scss'
-import '../src/app/app.js'
-import img from './assets/img/logo.png'
-
-console.log (img)
--- src/app/app.js
+++
@@ -1,4 +0,0 @@
-import { init } from "./component/app";
-
-init()
-
